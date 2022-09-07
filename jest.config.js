ea918717fff40e04d363461b3df53333d89ff1f9
Marcos R. Silva <w271592m@motorola.com>
2022-09-07 18:48:45 -0300

    chore: add jest

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  roots: ['<rootDir/src>'],
+  roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageProvider: 'v8',
   coverageDirectory: 'coverage',
-  testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
