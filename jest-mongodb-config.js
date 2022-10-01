0721629541cddcaefb500b6e874deec8471fd191
Marcos R. Silva <w271592m@motorola.com>
2022-10-01 16:58:05 -0300

    chore: add jest configs for mongodb

---
+++ jest-mongodb-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    mongodbMemoryServerOptions: {
+        instance: {
+            dbName: 'jest'
+        },
+        binary: {
+            version: '4.4.17',
+            skpMD5: true
+        },
+        autoStart: false
+    },
+};
