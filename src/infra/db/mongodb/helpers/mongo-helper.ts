82e793dbe69facfcc97a6542a4d25902b30f9839
Marcos R. Silva <w271592m@motorola.com>
2022-10-11 09:17:18 -0300

    refactor: connect to mongodb before integration tests

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -3,12 +3,8 @@ import { Collection, MongoClient } from 'mongodb'
 export const MongoHelper = {
     client: null as unknown as MongoClient,
 
-    async connect(url: string): Promise<void> {
-        // @ts-ignore
-        this.client = await MongoClient.connect(process.env.MONGO_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        })
+    async connect(uri: string): Promise<void> {
+        this.client = await MongoClient.connect(uri)
     },
 
     async disconnect(): Promise<void> {
